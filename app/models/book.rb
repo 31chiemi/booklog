24ef09c0f2096cef25108fc9d2455626f29e94b6
31chiemi <31chiemi@gmail.com>
2023-04-10 19:52:11 +0900

    ジャンルの追加

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,8 @@
 class Book < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :genre
+
+  validates :title, presence: true
+  validates :genre_id, numericality: { other_than: 0 , message: "can't be blank"}
+  validates :image, presence: true
 end
