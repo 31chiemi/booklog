3583cc4bdbf6c2557837c2bfa607aeb7f77cd0e8
31chiemi <31chiemi@gmail.com>
2023-04-11 12:04:20 +0900

    デバック機能の導入

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,8 +3,22 @@ class BooksController < ApplicationController
     @books = Book.all
   end
 
-  def index
+  def new
     @book = Book.new
+  end
+
+  def create
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+  
+  private
+  def book_params
+    params.require(:book).permit(:title, :content, :genre_id, :image).merge(user_id: current_user.id)
   end
 
 end
