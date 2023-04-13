362a2e9b25c0599e09762b80380bd94c569be9e8
31chiemi <31chiemi@gmail.com>
2023-04-13 16:07:02 +0900

    タイムゾーン削除

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,11 +2,10 @@ class UsersController < ApplicationController
   def show
     user = User.find(params[:id])
     @nickname = user.nickname
-    @age = user.age
     @books = user.books
-    
-    def age
-      (Time.zone.now - birthday.to_time) / 1.year.seconds
-    end
+    #@age = user.age
+    #def age
+      #(Time.zone.now - birthday.to_time) / 1.year.seconds
+    #end
   end
 end
